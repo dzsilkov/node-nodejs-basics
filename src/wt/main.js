145558e64d46fb10369fe6ed525c83569fc234b2
dzianis <dzsilkov@gmail.com>
2024-01-28 23:20:53 +0100

    completed wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const workerPath = resolve(__dirname, 'worker.js');
+
+
 const performCalculations = async () => {
-    // Write your code here
+    const initialValue = 10;
+    const workersP = cpus().map((cp, idx) => {
+            return new Promise(resolve => {
+                const worker = new Worker(workerPath, { workerData: initialValue + idx });
+                worker.on('message', (data) => resolve({ status: 'resolved', data }));
+                worker.on('error', () => resolve({ status: 'error', data: null }));
+            });
+        }
+    );
+    const result = await Promise.all(workersP);
+    console.log(result);
 };
 
 await performCalculations();
